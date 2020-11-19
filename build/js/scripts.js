a3eb131e8caf0ba17da7fa14372a0b7ab295e8d0
Edinson <edinson.ningles@gmail.com>
2020-11-19 11:29:01 +0100

    Add output

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -1,4 +1,4 @@
-!function(e){var t={};function n(r){if(t[r])return t[r].exports;var s=t[r]={i:r,l:!1,exports:{}};return e[r].call(s.exports,s,s.exports,n),s.l=!0,s.exports}n.m=e,n.c=t,n.d=function(e,t,r){n.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:r})},n.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},n.t=function(e,t){if(1&t&&(e=n(e)),8&t)return e;if(4&t&&"object"==typeof e&&e&&e.__esModule)return e;var r=Object.create(null);if(n.r(r),Object.defineProperty(r,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var s in e)n.d(r,s,function(t){return e[t]}.bind(null,s));return r},n.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return n.d(t,"a",t),t},n.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},n.p="",n(n.s=0)}({"./src/js/base/variables.js":
+!function(e){var t={};function n(r){if(t[r])return t[r].exports;var o=t[r]={i:r,l:!1,exports:{}};return e[r].call(o.exports,o,o.exports,n),o.l=!0,o.exports}n.m=e,n.c=t,n.d=function(e,t,r){n.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:r})},n.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},n.t=function(e,t){if(1&t&&(e=n(e)),8&t)return e;if(4&t&&"object"==typeof e&&e&&e.__esModule)return e;var r=Object.create(null);if(n.r(r),Object.defineProperty(r,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var o in e)n.d(r,o,function(t){return e[t]}.bind(null,o));return r},n.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return n.d(t,"a",t),t},n.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},n.p="",n(n.s=0)}({"./src/js/base/variables.js":
 /*!**********************************!*\
   !*** ./src/js/base/variables.js ***!
   \**********************************/
@@ -6,11 +6,11 @@
 /*!***********************************!*\
   !*** ./src/js/components/test.js ***!
   \***********************************/
-/*! exports provided: default */function(e,t,n){"use strict";n.r(t);t.default=class{constructor(e,t){this.name=e,this.age=t}getInfo(){return`${this.name} is ${this.age} years old.`}}},"./src/js/scripts.js":
+/*! exports provided: default */function(e,t,n){"use strict";n.r(t);t.default=class{constructor(e,t){this.name=e,this.age=t}getInfo(){return`${this.name} and he is ${this.age} years old`}}},"./src/js/scripts.js":
 /*!***************************!*\
   !*** ./src/js/scripts.js ***!
   \***************************/
-/*! no exports provided */function(e,t,n){"use strict";n.r(t);const r=new(n(/*! ./components/test */"./src/js/components/test.js").default)("John",35);console.log(r.getInfo())},0:
+/*! no exports provided */function(e,t,n){"use strict";n.r(t);const r=new(n(/*! ./components/test */"./src/js/components/test.js").default)("Edinson is Software Enginure from Microsof",36);console.log(r.getInfo())},0:
 /*!****************************************************************************************!*\
   !*** multi ./src/js/scripts.js ./src/js/base/variables.js ./src/js/components/test.js ***!
   \****************************************************************************************/
